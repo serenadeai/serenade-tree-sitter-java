30995f0b5cacdd6c8415a82414093415973fb633
Lawrence Wang <lawrence@serenade.ai>
2021-09-20 19:00:16 +0000

    trigger precommit

--- grammar.js
+++ grammar.js
@@ -352,11 +352,7 @@ module.exports = grammar({
 
     argument_list: $ => seq('(', commaSep($.expression), ')'),
 
-    method_reference: $ =>
-    
-
-    
-    seq(
+    method_reference: $ => seq (
       choice($._type, $.primary_expression, $.super),
       '::',
       optional($.type_arguments),
